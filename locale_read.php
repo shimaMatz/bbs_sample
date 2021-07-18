bebe6a0aabd1031c9c1af9cbb608e5373e1846d8
shimamatz <shimamatz@gmail.com>
2021-07-18 11:30:42 +0900

    Smarty対応コード

--- locale_read.php
+++
@@ -1,8 +0,0 @@
-<?php
-$filename  = 'locale/lang_ja.conf';
-// require_once('lang_ja.conf');
-
-$fp = fopen($filename, 'r');
-$txt = fgets($fp);
-$pieces = explode("=", $txt);
-fclose($fp);
--- tmp/9f4b3c2c4dfd7ce68de02e651006d6787bd50770_0.file.index.inc.php
+++ tmp/9f4b3c2c4dfd7ce68de02e651006d6787bd50770_0.file.index.inc.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-07-17 19:44:24
+/* Smarty version 3.1.39, created on 2021-07-18 02:12:43
   from '/var/www/html/views/index.inc' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_60f33318b1a878_02430295',
+  'unifunc' => 'content_60f38e1b359732_46969986',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '9f4b3c2c4dfd7ce68de02e651006d6787bd50770' => 
     array (
       0 => '/var/www/html/views/index.inc',
-      1 => 1626550551,
+      1 => 1626574361,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_60f33318b1a878_02430295 (Smarty_Internal_Template $_smarty_tpl) {
+function content_60f38e1b359732_46969986 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -38,21 +38,10 @@ function content_60f33318b1a878_02430295 (Smarty_Internal_Template $_smarty_tpl)
             </tr>
         </thead>
         <tbody>
-        <?php
-$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['people']->value, 'p', false, 'k');
-$_smarty_tpl->tpl_vars['p']->do_else = true;
-if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['k']->value => $_smarty_tpl->tpl_vars['p']->value) {
-$_smarty_tpl->tpl_vars['p']->do_else = false;
-?>
             <tr>
-                <td><?php echo $_smarty_tpl->tpl_vars['k']->value;?>
-</td>
-                <td><?php echo $_smarty_tpl->tpl_vars['p']->value;?>
+                <td><?php echo $_smarty_tpl->tpl_vars['title1']->value;?>
 </td>
             </tr>
-        <?php
-}
-$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
         </tbody>
     </table>
 </body>
